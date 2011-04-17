00f9580ce762f5e4c559b3fde664f5162680b08b
Outsider <outsideris@gmail.com>
2011-04-18 03:17:43 +0900

    Stylus 도입

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
