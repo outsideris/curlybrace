1f1be74382e754816c3d77fc49eb208e3a33d145
Outsider <outsideris@gmail.com>
2011-05-28 13:20:46 +0900

    서비스별 인증키값 추가

---
+++ conf/authconf.js
@@ -0,0 +1,17 @@
+module.exports = {
+    facebook: {
+        appId: '176737185714840'
+      , appSecret: '2ba8334436f52e84908bf69679e9c9f6'
+    }
+  , twitter: {
+        consumerKey: 'g74Oq12GHvtDKgN0ivrepA'
+      , consumerSecret: 'oZRjDVVUmPo0IvFpvmdu9fP2eF3ATKULQHnvC5io6vY'
+    }
+  , google: {
+        clientId: '330800073515.apps.googleusercontent.com'
+      , clientSecret: 'CP7GNxJ5I31qllbcqKWojw5i'
+    }
+  , me2day: {
+        key : '6c8bc00661adf619c93e4282f0faffe3'
+    }
+};
