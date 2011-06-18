a8e548cb31b56a9a76596f75b900cdf3a1a359f1
Outsider <outsideris@gmail.com>
2011-06-18 18:37:52 +0900

    들여쓰기 수정
    findOrCreateUser 에서 객체 리턴하도록 수정

--- utils/everyauth.js
+++ utils/everyauth.js
@@ -31,16 +31,19 @@ everyauth
     })
     .redirectPath('/join');
 
-everyauth.google
-  .myHostname('http://curlybrace.com:3000')
-  .appId(conf.google.clientId)
-  .appSecret(conf.google.clientSecret)
-  .scope('https://www.google.com/m8/feeds/')
-  .findOrCreateUser( function (sess, accessToken, extra, googleUser) {
-    googleUser.refreshToken = extra.refresh_token;
-    googleUser.expiresIn = extra.expires_in;
-    return usersByGoogleId[googleUser.id] || (usersByGoogleId[googleUser.id] = googleUser);
-  })
-  .redirectPath('/join');
+everyauth
+  .google
+    .myHostname('http://curlybrace.com:3000')
+    .appId(conf.google.clientId)
+    .appSecret(conf.google.clientSecret)
+    .scope('https://www.google.com/m8/feeds/')
+    .findOrCreateUser( function (sess, accessToken, extra, googleUser) {
+      console.log('google find');
+      googleUser.refreshToken = extra.refresh_token;
+      googleUser.expiresIn = extra.expires_in;
+      return googleUser;
+      //return usersByGoogleId[googleUser.id] || (usersByGoogleId[googleUser.id] = googleUser);
+    })
+     .redirectPath('/join');
   
 // everyauth.me2day
