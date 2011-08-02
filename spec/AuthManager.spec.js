638b9d87097789bfc51947d873c2385dd0e69128
Outsider <outsideris@gmail.com>
2011-08-03 03:02:54 +0900

    fixed jasmine spec but not working

--- spec/AuthManager.spec.js
+++ spec/AuthManager.spec.js
@@ -1,10 +1,35 @@
 var authmanager = require('../libs/authmanager')
-  , fixture = require('./fixture');
+  , fixture = require('./fixture')
+  , CONST = require('../conf/constant')
+  , Mongolian = require('mongolian');
 
 describe('authmanager', function() {
+  var server = new Mongolian(CONST.MONGODB_HOST + ':' + CONST.MONGODB_POST);
+  var db = server.db(CONST.MONGODB_DB);
+  var users = db.collection(CONST.MONGODB_COLLECTION);
+    
+  beforeEach(function() {
+     users.insert(fixture.twitter);
+     console.log('before');
+  });
+  
+  afterEach(function() {
+     users.remove();
+     console.log('after');
+  });
+  
   it('should addNewAcount for Twitter be passed', function() {
     var result = authmanager.addNewAcount(fixture.twitter, 'testname', function(result) {
+      console.log(result);
       expect(result).toBeTruthy();
+      console.log('test 1');
+    });
+  });
+  
+  it('should find user in twitter', function() {
+    authmanager.findAcountByid(fixture.twitter.userId, 'twitter', function(user) {
+      expect(user.nickname).toEqual(fixture.twitter.nickname);
+      console.log('test 2');
     });
   });
 });
