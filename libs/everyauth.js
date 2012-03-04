2e97228f8f5a9fc29cf998af5595e8fa9f6c173b
Outsider <outsideris@gmail.com>
2012-03-05 02:57:11 +0900

    facebook 인증 추가

--- libs/everyauth.js
+++ libs/everyauth.js
@@ -1,9 +1,12 @@
 var everyauth = module.exports = require('everyauth')
   , conf = require('../conf/authconf')
   , CONST = require('../conf/constant')
-  , authmanager = require('./authmanager')
+  , authManager = require('./authManager')
   , Promise = everyauth.Promise
-  , http = require('http');
+  , http = require('http')
+  , AuthOriginEnum = require('../conf/enums').AuthOriginEnum;
+
+var util = require('util');
 
 everyauth.debug = true;
 
@@ -12,70 +15,81 @@ var usersByTwitId = {};
 var usersByGoogleId = {};
 var usersByMe2dayId = {};
 
-everyauth
-  .facebook
-    .myHostname(CONST.HOST)
-    .appId(conf.facebook.appId)
-    .appSecret(conf.facebook.appSecret)
-    .findOrCreateUser( function (session, accessToken, accessTokenExtra, fbUserMetadata) {
-      return usersByFbId[fbUserMetadata.id] ||
-        (usersByFbId[fbUserMetadata.id] = fbUserMetadata);
-    })
-    .redirectPath('/join');
+everyauth.facebook
+  .appId(conf.facebook.appId)
+  .appSecret(conf.facebook.appSecret)
+  .findOrCreateUser( function (session, accessToken, accessTokenExtra, fbUserMetadata) {
+    clog.debug('session: ' + util.inspect(session));
+    clog.debug('accessToken: ' + util.inspect(accessToken));
+    clog.debug('accessTokenExtra: ' + util.inspect(accessTokenExtra));
+    clog.debug('fbUserMetadata: ' + util.inspect(fbUserMetadata));
+    var promise = this.Promise();
+    authManager.findAcountBy(fbUserMetadata.id, AuthOriginEnum.facebook, function(err, user) {
+      if (err) return promise.fail(err);
+      clog.debug(user);
+      if (user) {
+        promise.fulfill(user);
+      } else {
+        authManager.addNewAcount(fbUserMetadata, AuthOriginEnum.facebook, function(err, insertedUser) {
+          promise.fulfill(insertedUser);
+        });
+      }
+    });
+    return promise;
+  })
+  .redirectPath('/');
 
-everyauth
-  .twitter
-    .myHostname(CONST.HOST)
-    .consumerKey(conf.twitter.consumerKey)
-    .consumerSecret(conf.twitter.consumerSecret)
-    .findOrCreateUser( function (sess, accessToken, accessSecret, twitUser) {
-      var promise = this.Promise();
-      authmanager.findAcountByid(twitUser.id, 'twitter', function (err, user) {
-        if (err) return promise.fail(err);
-        //user ? promise.fulfill(user) : promise.fulfill({});
-        if (user) {
-          console.log('user');
-          promise.fulfill(user);
-        } else {
-          console.log('none');
-          promise.fulfill({})
-        }
-      });
-      return promise;
-    })
-    .redirectPath('/join');
+everyauth.twitter
+  .consumerKey(conf.twitter.consumerKey)
+  .consumerSecret(conf.twitter.consumerSecret)
+  .findOrCreateUser( function (sess, accessToken, accessSecret, twitUser) {
+    var promise = this.Promise();
+    authmanager.findAcountByid(twitUser.id, 'twitter', function (err, user) {
+      if (err) return promise.fail(err);
+      //user ? promise.fulfill(user) : promise.fulfill({});
+      if (user) {
+        clog.info('user');
+        promise.fulfill(user);
+      } else {
+        clog.info('none');
+        promise.fulfill({})
+      }
+    });
+    return promise;
+  })
+  .redirectPath('/join');
 
 everyauth
   .google
-    .myHostname(CONST.HOST)
-    .appId(conf.google.clientId)
-    .appSecret(conf.google.clientSecret)
-    .scope('https://www.google.com/m8/feeds/')
-    .findOrCreateUser( function (sess, accessToken, extra, googleUser) {
-      console.log('google find');
-      googleUser.refreshToken = extra.refresh_token;
-      googleUser.expiresIn = extra.expires_in;
-      return googleUser;
-      //return usersByGoogleId[googleUser.id] || (usersByGoogleId[googleUser.id] = googleUser);
-    })
-     .redirectPath('/join');
-  
+  .myHostname(CONST.HOST)
+  .appId(conf.google.clientId)
+  .appSecret(conf.google.clientSecret)
+  .scope('https://www.google.com/m8/feeds/')
+  .findOrCreateUser( function (sess, accessToken, extra, googleUser) {
+    console.log('google find');
+    googleUser.refreshToken = extra.refresh_token;
+    googleUser.expiresIn = extra.expires_in;
+    return googleUser;
+    //return usersByGoogleId[googleUser.id] || (usersByGoogleId[googleUser.id] = googleUser);
+  })
+  .redirectPath('/join');
+
 everyauth.me2day = function(callback) {
   var options = {
-     host: 'me2day.net'
-   , port: 80
-   , path: '/api/get_auth_url.json?akey=' + conf.me2day.key
+    host: 'me2day.net'
+    , port: 80
+    , path: '/api/get_auth_url.json?akey=' + conf.me2day.key
   };
   http.get(options, function(res) {
-      var body = '';
-      res.on('data', function(data) {
-        body += data; 
-      });
-      res.on('end', function() {
-        body = JSON.parse(body);
-        callback(null, body.url, body.token);
-      });
-    }).on('error', function(e) {
+    var body = '';
+    res.on('data', function(data) {
+      body += data;
+    });
+    res.on('end', function() {
+      body = JSON.parse(body);
+      callback(null, body.url, body.token);
+    });
+  }).on('error', function(e) {
       callback(e);
     });
 };
