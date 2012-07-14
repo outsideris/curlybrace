083f8e893fa4ea49e12e556e626c52a20fef6135
Outsider <outsideris@gmail.com>
2012-07-14 20:30:14 +0900

    질문작성 화면에서 작성/미리보기 탭 작성
    * 상세보기 화면에서만 소셜버튼이 보이도록 수정
    
    * markdown.js 추가 ( https://github.com/evilstreak/markdown-js )
    
    * 작성에 출력된 내용을 미리보기에서 HTML으로 출력

--- public/javascripts/commons.js
+++ public/javascripts/commons.js
@@ -5,37 +5,56 @@ $(document).ready(function() {
   $('.votecount').tooltip(tooltipOpt);
   $('.answercount').tooltip(tooltipOpt);
 
-  //Twitter Button
-  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+  if ($('.socialbox').length) {
+    //Twitter Button
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
-  //Facebook Like Button
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '176737185714840', // App ID
-      channelUrl : '//curlybrace.com:3000/html/channel.html', // Channel File
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
+    //Facebook Like Button
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '176737185714840', // App ID
+        channelUrl : '//curlybrace.com:3000/html/channel.html', // Channel File
+        status     : true, // check login status
+        cookie     : true, // enable cookies to allow the server to access the session
+        xfbml      : true  // parse XFBML
+      });
+
+      // Additional initialization code here
+    };
+
+    // Load the SDK Asynchronously
+    (function(d){
+      var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement('script'); js.id = id; js.async = true;
+      js.src = "//connect.facebook.net/en_US/all.js";
+      ref.parentNode.insertBefore(js, ref);
+    }(document));
+
+    //Google+ Button
+    window.___gcfg = {lang: 'ko'};
+
+    (function() {
+      var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+      po.src = 'https://apis.google.com/js/plusone.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    })();
+  }
+
+  if ($('.question-form').length) {
+    var editor = $('#editor').find('textarea');
+    var preview = $('#preview-box');
+
+    $('#editor-tab').tab('show');
+    $('#editor-tab').click(function(e) {
+      e.preventDefault();
+      $(this).tab('show');
     });
+    $('#preview-tab').click(function(e) {
+      e.preventDefault();
+      $(this).tab('show');
 
-    // Additional initialization code here
-  };
-
-  // Load the SDK Asynchronously
-  (function(d){
-    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement('script'); js.id = id; js.async = true;
-    js.src = "//connect.facebook.net/en_US/all.js";
-    ref.parentNode.insertBefore(js, ref);
-  }(document));
-
-  //Google+ Button
-  window.___gcfg = {lang: 'ko'};
-
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/plusone.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
+      preview.html(markdown.toHTML(editor.val()));
+    });
+  }
 });
