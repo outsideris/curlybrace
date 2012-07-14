f860ee62740185727a7e8d6a57617013931c50ac
Outsider <outsideris@gmail.com>
2012-07-14 20:53:30 +0900

    jade를 레이아웃대신 템플릿상속방식으로 변경
    * 특정 페이지에서만 필요한 스크립트 위해 템플릿 상속이 더 유용함.

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ var app = module.exports = express.createServer();
 app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('view options', { layout: false });
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'htuayreve'}));
