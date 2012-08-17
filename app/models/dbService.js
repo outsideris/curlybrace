b29354bdd509baa93756f9706f5e4b96026fbd25
Outsider <outsideris@gmail.com>
2012-08-18 03:09:28 +0900

    DB의 의존성주입으로 인해서 여러번 접속되는 문제 수정

--- app/models/dbService.js
+++ app/models/dbService.js
@@ -2,31 +2,33 @@ var Mongolian = require('mongolian')
   , env = require('../../conf/config').env;
 
 module.exports = {
-    db: null
+  db: null
   , users: null
   , tags: null
   , init: function(opt) {
-      opt = opt || {};
-      var config = {
-          host: opt.host || env.MONGODB_HOST
-        , port: opt.port || env.MONGODB_PORT
-        , dbName: opt.dbName || env.MONGODB_DB
-        , users: opt.users || env.MONGODB_COLLECTION_USERS
-        , tags: opt.tags || env.MONGODB_COLLECTION_TAGS
-      };
+    opt = opt || {};
+    var config = {
+      host: opt.host || env.MONGODB_HOST
+      , port: opt.port || env.MONGODB_PORT
+      , dbName: opt.dbName || env.MONGODB_DB
+      , users: opt.users || env.MONGODB_COLLECTION_USERS
+      , tags: opt.tags || env.MONGODB_COLLECTION_TAGS
+    };
 
+    if (!this.db) {
       this.db = new Mongolian(config.host + ':' + config.port, {
         log: {
-            debug: function(message){ console.log(message) }
+          debug: function(message){ console.log(message) }
           , info:  function(message){ console.log(message) }
           , warn:  function(message){ console.log(message) }
           , error: function(message){ console.log(message) }
         }
       }).db(config.dbName);
-
-      this.users = this.db.collection(config.users);
-      this.tags = this.db.collection(config.tags);
-
-      return this;
     }
+
+    this.users = this.db.collection(config.users);
+    this.tags = this.db.collection(config.tags);
+
+    return this;
+  }
 };
