6300ebf24b839b5898f141e4663c055cc2df9091
Outsider <outsideris@gmail.com>
2013-05-26 20:37:13 +0900

    Question 테스트 케이스 추가

--- app/models/Question.scala
+++ app/models/Question.scala
@@ -19,7 +19,9 @@ case class Question(
   contents: String,
   regdate: DateTime = DateTime.now,
   voteup: Int = 0,
-  votedown: Int = 0
+  votedown: Int = 0,
+  answerCount: Int = 0,
+  commentsCount: Int = 0
 )
 
 object Questions extends Table[Question]("questions") {
@@ -27,12 +29,18 @@ object Questions extends Table[Question]("questions") {
   def title = column[String]("title", O.NotNull)
   def contents = column[String]("contents", O.NotNull)
   def regdate = column[DateTime]("regdate", O.NotNull)
-  def voteup = column[Int]("voteup")
-  def votedown = column[Int]("votedown")
+  def voteup = column[Int]("vote_up")
+  def votedown = column[Int]("vote_down")
+  def answerCount = column[Int]("answer_count")
+  def commentsCount = column[Int]("comments_count")
   // FIXME: add user, tag, voting
-  def * = id.? ~ title ~ contents ~ regdate ~ voteup ~ votedown  <> (Question, Question.unapply _)
+  def * = id.? ~ title ~ contents ~ regdate ~ voteup ~ votedown ~ answerCount  ~ commentsCount  <> (Question, Question.unapply _)
 
   def add(question: Question)(implicit session: Session) = {
+    // validation
+    require(question.title != null && !question.title.trim.isEmpty, "title")
+    require(question.contents != null && !question.contents.trim.isEmpty, "contents")
+
     Questions.insert(question)
   }
 
--- test/models/QuestionSpec.scala
+++ test/models/QuestionSpec.scala
@@ -6,6 +6,7 @@ import org.scalatest.matchers.ShouldMatchers
 import scala.slick.driver.H2Driver.simple._
 
 import Database.threadLocalSession
+import java.lang.IllegalArgumentException
 
 /**
  * Copyright (c) 2013 JeongHoon Byun aka "Outsider", <http://blog.outsider.ne.kr/>
@@ -24,6 +25,12 @@ class QuestionSpec extends FunSpec with BeforeAndAfter with ShouldMatchers {
     "질문 내용"
   )
 
+//  before {
+//  }
+//
+//  after {
+//  }
+
   describe("add") {
     it("질문을 저장한다") {
       Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver") withSession {
@@ -37,6 +44,78 @@ class QuestionSpec extends FunSpec with BeforeAndAfter with ShouldMatchers {
         q.get.contents should equal (questionFixture(Some(2)).contents)
         q.get.voteup should equal (0)
         q.get.votedown should equal (0)
+      }
+    }
+    it("제목이 null 저장되지 않는다") {
+      Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver") withSession {
+        // given
+        val fixture = Question(Some(1), null, "질문 내용")
+        Questions.ddl.create
+        // then
+        intercept[IllegalArgumentException] {
+          // when
+          Questions.add(fixture)
+        }
+      }
+    }
+    it("제목이 빈값이면 저장되지 않는다") {
+      Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver") withSession {
+        // given
+        val fixture = Question(Some(1), "", "질문 내용")
+        Questions.ddl.create
+        // then
+        intercept[IllegalArgumentException] {
+          // when
+          Questions.add(fixture)
+        }
+      }
+    }
+    it("제목이 공백이면 저장되지 않는다") {
+      Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver") withSession {
+        // given
+        val fixture = Question(Some(1), "   ", "질문 내용")
+        Questions.ddl.create
+        // then
+        intercept[IllegalArgumentException] {
+          // when
+          Questions.add(fixture)
+        }
+      }
+    }
+    it("내용이 null 저장되지 않는다") {
+      Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver") withSession {
+        // given
+        val fixture = Question(Some(1), "제목", null)
+        Questions.ddl.create
+        // then
+        intercept[IllegalArgumentException] {
+          // when
+          Questions.add(fixture)
+        }
+      }
+    }
+    it("내용이 빈값이면 저장되지 않는다") {
+      Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver") withSession {
+        // given
+        val fixture = Question(Some(1), "제목", "")
+        Questions.ddl.create
+        // then
+        intercept[IllegalArgumentException] {
+          // when
+          Questions.add(fixture)
+        }
+      }
+    }
+    it("내용이 공백이면 저장되지 않는다") {
+      Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver") withSession {
+        // given
+        val fixture = Question(Some(1), "제목", "  ")
+        Questions.ddl.create
+        // then
+        intercept[IllegalArgumentException] {
+          // when
+          Questions.add(fixture)
+        }
       }
     }
   }
