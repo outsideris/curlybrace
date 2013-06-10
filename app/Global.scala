e6e76cd5976f64306ef07cc0211ca80a45212190
Outsider <outsideris@gmail.com>
2013-06-10 21:02:12 +0900

    play 구동시 모델에 대한 테이블 생성

--- app/Global.scala
+++ app/Global.scala
@@ -6,7 +6,8 @@ import Database.threadLocalSession
 
 import play.api.Application
 import play.api.Play.current
-import models.{Questions, Tags}
+import models._
+import models.users._
 
 /**
  * Copyright (c) 2013 JeongHoon Byun aka "Outsider", <http://blog.outsider.ne.kr/>
@@ -26,7 +27,13 @@ object Global extends GlobalSettings {
     database .withSession {
       (
         Questions.ddl ++
-        Tags.ddl
+        Answers.ddl ++
+        Tags.ddl ++
+        QuestionsToTags.ddl ++
+        Comments.ddl ++
+        Votes.ddl ++
+        Users.ddl ++
+        Socials.ddl
       ).create
       // insert initial tags
       Tags.init
